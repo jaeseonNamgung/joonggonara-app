4b96c79472ec630c382cb7bc37e7d8cb6e13b937
jaeseonNamgung <sunnamgung8@naver.com>
2024-04-28 17:17:22 +0800

    Member Repository 구현
    existByEmail -> existUserId 변경
    existByUserNameAndVerificationTypeValue 구현

--- src/main/java/com/hit/joonggonara/repository/login/querydsl/MemberQueryDslImpl.java
+++ src/main/java/com/hit/joonggonara/repository/login/querydsl/MemberQueryDslImpl.java
@@ -1,5 +1,8 @@
 package com.hit.joonggonara.repository.login.querydsl;
 
+import com.hit.joonggonara.common.type.VerificationType;
+import com.hit.joonggonara.service.login.condition.VerificationCondition;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
@@ -11,11 +14,34 @@ public class MemberQueryDslImpl implements MemberQueryDsl{
     private final JPAQueryFactory jpaQueryFactory;
 
     @Override
-    public boolean existByEmail(String email) {
+    public boolean existByUserId(String userId) {
         Integer exist = jpaQueryFactory.selectOne()
                 .from(member)
-                .where(member.email.eq(email))
+                .where(member.userId.eq(userId))
                 .fetchFirst();
         return exist != null;
     }
+
+    @Override
+    public boolean existByUserNameAndVerificationTypeValue(VerificationCondition condition, VerificationType verificationType) {
+        Integer exist = jpaQueryFactory.selectOne()
+                .from(member)
+                .where(member.name.eq(condition.username()), verificationType(condition, verificationType))
+                .fetchOne();
+        return exist != null;
+    }
+
+    private BooleanExpression verificationType(VerificationCondition condition, VerificationType verificationType) {
+
+        if(VerificationType.ID_SMS.equals(verificationType)){
+            return member.phoneNumber.eq(condition.verificationCode());
+        } else if(VerificationType.ID_EMAIL.equals(verificationType)){
+            return member.email.eq(condition.verificationCode());
+        } else if(VerificationType.PASSWORD_SMS.equals(verificationType)){
+            return member.phoneNumber.eq(condition.verificationCode()).and(member.userId.eq(condition.userId()));
+        } else{
+            return member.email.eq(condition.verificationCode()).and(member.userId.eq(condition.userId()));
+        }
+    }
+
 }
